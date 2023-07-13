9acfcafcf8f582d6ff9e85c7434e607dd8204b77
Solomon Akhahon <Solomonakhahon@gmail.com>
2023-07-13 15:59:33 +0100

    inital commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("hello")
+height = input("Enter height:")
+print("height is: " + height)
+height=float(height)
+weight = input("Enter weight:")
+print("weight is: " + weight)
+weight=float(weight)
+
+bmi=weight / height **2 
+print("bmi is" + str(bmi))
